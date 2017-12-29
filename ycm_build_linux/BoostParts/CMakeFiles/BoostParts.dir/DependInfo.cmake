910dac2ae265eb4896468d4dd447df4b188ddaf1
尹萌 <xiaoyin199@sp-wudaji.hxsd.local>
2017-12-29 17:05:20 +0800

    youcomplate支持

---
+++ ycm_build_linux/BoostParts/CMakeFiles/BoostParts.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/filesystem/src/codecvt_error_category.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/filesystem/src/codecvt_error_category.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/filesystem/src/operations.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/filesystem/src/operations.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/filesystem/src/path.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/filesystem/src/path.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/filesystem/src/path_traits.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/filesystem/src/path_traits.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/filesystem/src/portability.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/filesystem/src/portability.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/filesystem/src/unique_path.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/filesystem/src/unique_path.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/filesystem/src/utf8_codecvt_facet.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/filesystem/src/utf8_codecvt_facet.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/filesystem/src/windows_file_codecvt.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/filesystem/src/windows_file_codecvt.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/converter/arg_to_python_base.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/converter/arg_to_python_base.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/converter/builtin_converters.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/converter/builtin_converters.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/converter/from_python.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/converter/from_python.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/converter/registry.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/converter/registry.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/converter/type_id.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/converter/type_id.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/dict.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/dict.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/errors.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/errors.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/exec.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/exec.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/import.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/import.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/list.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/list.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/long.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/long.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/module.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/module.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object/class.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object/class.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object/enum.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object/enum.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object/function.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object/function.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object/function_doc_signature.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object/function_doc_signature.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object/inheritance.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object/inheritance.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object/iterator.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object/iterator.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object/life_support.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object/life_support.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object/pickle_support.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object/pickle_support.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object/stl_iterator.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object/stl_iterator.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object_operators.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object_operators.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/object_protocol.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/object_protocol.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/slice.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/slice.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/str.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/str.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/tuple.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/tuple.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/python/src/wrapper.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/python/src/wrapper.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/c_regex_traits.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/c_regex_traits.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/cpp_regex_traits.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/cpp_regex_traits.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/cregex.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/cregex.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/fileiter.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/fileiter.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/icu.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/icu.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/instances.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/instances.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/posix_api.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/posix_api.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/regex.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/regex.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/regex_debug.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/regex_debug.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/regex_raw_buffer.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/regex_raw_buffer.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/regex_traits_defaults.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/regex_traits_defaults.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/static_mutex.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/static_mutex.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/usinstances.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/usinstances.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/w32_regex_traits.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/w32_regex_traits.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/wc_regex_traits.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/wc_regex_traits.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/wide_posix_api.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/wide_posix_api.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/regex/src/winstances.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/regex/src/winstances.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts/libs/system/src/error_code.cpp" "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/libs/system/src/error_code.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "YCMD_CORE_VERSION=31"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts"
+  "/usr/local/include/python2.7"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ ycm_build_linux/ycm/CMakeFiles/ycm_core.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/Candidate.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/Candidate.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/CandidateRepository.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/CandidateRepository.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/IdentifierCompleter.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/IdentifierCompleter.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/IdentifierDatabase.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/IdentifierDatabase.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/IdentifierUtils.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/IdentifierUtils.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/LetterNode.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/LetterNode.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/LetterNodeListMap.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/LetterNodeListMap.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/PythonSupport.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/PythonSupport.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/Result.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/Result.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/Utils.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/Utils.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/versioning.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/versioning.cpp.o"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/ycm_core.cpp" "/root/.ycm_build/ycm/CMakeFiles/ycm_core.dir/ycm_core.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "YCMD_CORE_VERSION=31"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/.ycm_build/BoostParts/CMakeFiles/BoostParts.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts"
+  "/usr/local/include/python2.7"
+  "/root/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/llvm/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
